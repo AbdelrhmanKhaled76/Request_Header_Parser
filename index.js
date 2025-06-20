5f351d85dead33fc341e66787f6288ba2ef67a5b
Abdelrhman khaled Mohammed <124658552+AbdelrhmanKhaled76@users.noreply.github.com>
2025-06-21 00:34:44 +0300

    updating head parser project

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname + "./public/index.html");
 });
 
-app.get('/api/:whoami',(req,res)=>{
-    const {whoami} = req.params;
-    if(whoami){
-        res.json({ipaddress : req.ip
-        ,language : req.headers['accept-language'],
-        software : req.headers['user-agent']})
-    }
-    res.status(404).send("not found")
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
 });
 
 var listener = app.listen(process.env.PORT || 3000, ()=>{
